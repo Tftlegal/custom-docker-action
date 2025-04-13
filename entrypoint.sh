48d958a4ef8e7b7277de56122df09197debfaeb2
Tftlegal <94695082+Tftlegal@users.noreply.github.com>
2025-04-13 16:10:29 +0300

    Update test entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-#curl -s "https://get.sytes.ru/json/port_scan_results.json" | jq -r --arg ip "192.168.99.1" 'select(.ip? | index($ip)) | .open_ports| join(", ")'
-
-api_url="https://get.sytes.ru/json/port_scan_results.json"
+api_url="https://jsonplaceholder.typicode.com/users/${INPUT_USER_ID}"
 echo $api_url
-open_port=$(curl -s "${api_url}" | jq -r --arg ip "${INPUT_IP}" '.[] | select(.ip == $ip) | .open_ports[]')
-echo $open_port
 
-echo "::set-output name=open_port::$open_port"
+user_name=$(curl "${api_url}" | jq ".name")
+echo $user_name
+
+echo "::set-output name=user_name::$user_name"
 
 
