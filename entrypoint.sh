6679fc4efdf3ce9bca42902911a3e1cf2d7da71e
Tftlegal <94695082+Tftlegal@users.noreply.github.com>
2025-05-29 15:04:04 +0300

    Update service entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Используем правильную переменную для входа (без INPUT_ префикса)
-api_url="https://jsonplaceholder.typicode.com/users/$1"
+api_url="https://up.sytes.ru/api5.php?id=$1"
 echo "API URL: $api_url"
 
-user_name=$(curl -s "${api_url}" | jq -r ".name")
+user_name=$(curl -u 'user_data_json:jjlskdfhr489jskk@klsdjf' -s -X GET "${api_url}" | jq -r ".username")
 echo "User name: $user_name"
 
 # Новый формат вывода (старый ::set-output deprecated)
